757c93d1c45971d238365c6e96080516c181ac47
Nathan Morin <nathanamorin@gmail.com>
2014-07-24 14:48:37 -0400

    added handlers to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions provide a way to cache the inverse of a data element
+## and automatically eather get values from cache or calculate new values and put into cache.
 
-## Write a short comment describing this function
+## This function takes a matrix and provides the handling functions to get & set the matrix & mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y=matrix()){
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function(){
+                x
+        }
+        
+        setmean <- function(mean){
+                m <<- mean
+        }
+        
+        getmean <- function(){
+                m
+        }
+        
+        list(set=set,
+             get=get,
+             setmean=setmean,
+             getmean=getmean)
 }
 
 
